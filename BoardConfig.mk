2818e214862155e8d8643d7663662c1cb3be0cf8
Furrydaus <furrydaus@gmail.com>
2015-05-03 21:25:19 +0800

    Bringup for CM12

---
+++ BoardConfig.mk
@@ -0,0 +1,144 @@
+# Copyright (C) 2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# inherit from msm8960-common
+include device/sony/msm8960-common/BoardConfigCommon.mk
+
+TARGET_SPECIFIC_HEADER_PATH += device/sony/huashan/include
+
+# Kernel properties
+TARGET_KERNEL_SOURCE := kernel/sony/msm8x60
+TARGET_KERNEL_CONFIG := cm_viskan_huashan_defconfig
+
+# Platform
+TARGET_BOOTLOADER_BOARD_NAME := MSM8960
+BOARD_VENDOR_PLATFORM := viskan
+
+# Kernel information
+BOARD_KERNEL_BASE  := 0x80200000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_CMDLINE  := androidboot.hardware=qcom user_debug=31 androidboot.baseband=msm msm_rtb.filter=0x3F ehci-hcd.park=3 vmalloc=400M androidboot.selinux=permissive selinux=0
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
+
+# Use legacy MMAP for pre-lollipop blobs
+BOARD_USES_LEGACY_MMAP := true
+
+# Time
+BOARD_USES_QC_TIME_SERVICES := true
+
+# Dumpstate
+BOARD_LIB_DUMPSTATE := libdumpstate.sony
+
+# Wifi
+BOARD_HAS_QCOM_WLAN              := true
+BOARD_WLAN_DEVICE                := qcwcn
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+BOARD_HOSTAPD_DRIVER             := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wlan.ko"
+WIFI_DRIVER_MODULE_NAME          := "wlan"
+WIFI_DRIVER_FW_PATH_STA          := "sta"
+WIFI_DRIVER_FW_PATH_AP           := "ap"
+
+BOARD_USE_SONY_MACUPDATE := true
+
+BOARD_HARDWARE_CLASS := device/sony/huashan/cmhw
+
+# Camera
+USE_DEVICE_SPECIFIC_CAMERA := true
+
+# GPS
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := $(TARGET_BOARD_PLATFORM)
+TARGET_NO_RPC := true
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/sony/huashan/bluetooth
+
+# Legacy RIL
+BOARD_HAS_RIL_LEGACY_PAP := true
+
+# Needed for blobs
+TARGET_RELEASE_CPPFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
+
+# Vold
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
+
+# Custom boot
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+BOARD_CUSTOM_BOOTIMG_MK := device/sony/huashan/custombootimg.mk
+BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
+
+TARGET_RECOVERY_FSTAB := device/sony/huashan/rootdir/fstab.qcom
+RECOVERY_FSTAB_VERSION := 2
+
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_HAS_NO_SELECT_BUTTON := true
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := C5302,C5303,C5306,huashan
+
+# Audio
+BOARD_USES_LEGACY_ALSA_AUDIO := true
+TARGET_USES_QCOM_COMPRESSED_AUDIO := true
+BOARD_HAVE_NEW_QCOM_CSDCLIENT := true
+BOARD_HAVE_CSD_FAST_CALL_SWITCH := true
+BOARD_USES_FLUENCE_INCALL := true
+BOARD_USES_SEPERATED_AUDIO_INPUT := true
+BOARD_USES_SEPERATED_VOICE_SPEAKER_MIC := true
+
+# FM Radio
+QCOM_FM_ENABLED := true
+AUDIO_FEATURE_ENABLED_FM := true
+
+# Partition information
+BOARD_VOLD_MAX_PARTITIONS := 26
+
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x01400000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x01400000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1056964608
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 2147483648
+
+# Include common SE policies
+-include device/qcom/sepolicy/sepolicy.mk
+
+BOARD_SEPOLICY_DIRS += \
+    device/sony/huashan/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+    file_contexts \
+    property_contexts \
+    bootanim.te \
+    illumination.te \
+    init.te \
+    mac_update.te \
+    mediaserver.te \
+    platform_app.te \
+    property.te \
+    rmt_storage.te \
+    secchand.te \
+    setup_fs.te \
+    surfaceflinger.te \
+    system_app.te \
+    system_monitor.te \
+    system_server.te \
+    tad_static.te \
+    ta_qmi_service.te \
+    updatemiscta.te
+
+# inherit from the proprietary version
+-include vendor/sony/huashan/BoardConfigVendor.mk
